5fb4d3e785b34069d88dc9542771f364f5b8a3c6
Jack Zheng <jackzheng274@gmail.com>
2024-03-26 03:15:28 -0700

    ++

--- src/Pages/Individual/Individual.jsx
+++ src/Pages/Individual/Individual.jsx
@@ -1,11 +1,33 @@
+import { FaRegStar } from "react-icons/fa";
+
 import "./Individual.css";
 
 const Individual = () => {
   return (
     <div>
-      <div className="Image-Container">
-        <div className="Big-Picture"></div>
-        <div className="Small-Picture-Container"></div>
+      <div className="Page-Container">
+        <div className="Image-Container">
+          <div className="Big-Pic"></div>
+        </div>
+        <div className="Info-Container">
+          <div className="General-Info">
+            Title of Shirt
+            <div className="Stars">
+              <FaRegStar />
+              <FaRegStar />
+              <FaRegStar />
+              <FaRegStar />
+              <FaRegStar />
+            </div>
+            <div className="Cost">[Cost]</div>
+            <div className="Sizes">
+              <div className="Small">Small</div>
+              <div className="Medium">Medium</div>
+              <div className="Large">Large</div>
+            </div>
+            <div className="Cart">Add to Cart</div>
+          </div>
+        </div>
       </div>
     </div>
   );
