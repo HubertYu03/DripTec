c4bbbdb328f4db1e232a84f47e44a80fec31f084
hubbbbbbbbb <hubyu9234@gmail.com>
2024-04-15 11:09:00 -0700

    Tracking number search function added

--- src/Pages/TrackPackage/TrackPackage.jsx
+++ src/Pages/TrackPackage/TrackPackage.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+// importing dependencies
+import { useState } from "react";
+import { supabase } from "../../Client";
+
+// importing styles
+import "./TrackPackage.css";
 
 const TrackPackage = () => {
-  return <div></div>;
+  const [orderNum, setOrderNum] = useState("");
+  const [orderExists, setOrderExists] = useState(false);
+  const [order, setOrder] = useState({});
+
+  const handleSubmit = async () => {
+    let { data: Orders, error } = await supabase
+      .from("Orders")
+      .select("*")
+      .eq("trackingNumber", orderNum);
+    if (Orders.length > 0) {
+      console.log("Order exists");
+      setOrderNum("");
+      setOrder(Orders[0]);
+    } else {
+      console.log("Order does not exist");
+    }
+  };
+
+  return (
+    <div className="track-package-page-container">
+      <div className="track-package-title">Track Your Order</div>
+      <input
+        type="text"
+        placeholder="Your order number..."
+        className="track-number-input"
+        value={orderNum}
+        onChange={(event) => setOrderNum(event.target.value)}
+      />
+      <button className="track-button" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default TrackPackage;
