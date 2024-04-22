68dfcc05c0bca7d05ced3fe9b190031dce10e00c
Jack Zheng <jackzheng274@gmail.com>
2024-04-22 04:35:08 -0700

    Added SignUpPage to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
           <Route path="/shoppingCategories" element={<ShoppingCategories />} />
           <Route path="/individualProduct" element={<Individiual />} />
         </Routes>
